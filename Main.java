8bef73a862e95d0b8e99328184222c77eb93ccd3
khaledlim <khaledlimem7@gmail.com>
2020-03-21 15:23:28 +0100

    make modifications

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main
 {
     public static void main(String args[]) {
         System.out.println("Cristiano Ronaldo CR7...");
+		System.out.println("CORONA VIRUS...");
         System.out.println("CR7 plays in : Juventus, Real madrid and Manchester united...");
         for(int i=1; i<=13;i++){
             System.out.println("Real Madrid Champions Leagues..."+i);
